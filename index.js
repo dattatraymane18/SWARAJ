9b09d03a8d1ccb2ec698aaedda50eddf82c73dd8
Dattatray Mane <155941213+dattatraymane18@users.noreply.github.com>
2024-01-08 19:30:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+  res.send('Dattatray mane')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
